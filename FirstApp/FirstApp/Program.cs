546dec1d2bef9d75a68aec8705905ccc9281aeda
DEV01 <DEV01@GOJEONGHUN>
2018-06-05 16:19:27 +0900

    .

--- FirstApp/FirstApp/Program.cs
+++ FirstApp/FirstApp/Program.cs
@@ -14,7 +14,7 @@ namespace FirstApp
         [STAThread]
         static void Main()
         {
-            ///////////////
+            ///////////////****************************
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
